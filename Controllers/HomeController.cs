4ffa081a8765b8364d68da22c80842914c6fbcee
IrvinFrias <irvin.frias.unam@outlook.com>
2023-05-21 20:32:06 -0600

    We added a new section About

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,4 +33,11 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+
+    //Mis métodos:
+    public IActionResult About(){
+        return View();
+    }
 }
